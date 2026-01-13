3ac7605d2b1274f587e1bd08903e3877f162b1dd
alice <nguyentngazure@gmail.com>
2026-01-13 20:28:07 +0700

    Baby step (nested DS) of day-stream-task data model

---
+++ time.py
@@ -0,0 +1,14 @@
+#print a timeline
+
+#nested data structure: list TIMELINE chứa dictionary DAY chứa dictionary STREAM chứa list PROGRAM
+stream = {"Stream" + str(X): ["pro1", "pro2"] for X in range(1, 4)}
+stream2 = {"Stream" + str(X): ["pro1", "pro2"] for X in range(1, 4)}
+day = {"Day" + str(X): stream for X in range(1, 3)}
+# day1 = ["Stream" + str(X) for X in range(1, 4)]
+# day2 = ["Stream" + str(X) for X in range(1, 4)]
+timeline = [
+    day
+]
+print (timeline)
+
+#nếu là dictionary thì phải dùng : thay vì = trong khai báo
